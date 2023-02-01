c43766d347074329e0f926df15a3d04b7d0fd29b
arigunawanj <arigunawanjatmiko@gmail.com>
2023-02-01 07:25:42 +0700

    Template

--- routes/web.php
+++ routes/web.php
@@ -21,7 +21,7 @@ use App\Http\Controllers\PenjualanController;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('layouts.template');
 });
 
 Route::resource('barang', BarangController::class);
