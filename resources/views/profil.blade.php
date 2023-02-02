44ee11228b9e21497413303bc74d4cc0bb1827dc
arigunawanj <arigunawanjatmiko@gmail.com>
2023-02-02 23:56:35 +0700

    Tampilan Profil dan Route

--- resources/views/profil.blade.php
+++ resources/views/profil.blade.php
@@ -2,89 +2,51 @@
 
 @section('title', 'Profil')
 @section('content')
-<div class="content-wrapper">
-    <!-- Content -->
-    <div class="container-xxl flex-grow-1 container-p-y">
-    </div>
-    <!-- Modal -->
-    <div class="modal fade" id="tambahData" tabindex="-1" aria-hidden="true">
-        <div class="modal-dialog modal-dialog-centered" role="document">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <h5 class="modal-title" id="modalCenterTitle">Tambah Data Customer</h5>
-                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                </div>
-                <div class="modal-body">
-                    <div class="row">
-                        <form>
-                            <div class="row mb-3">
-                                <label class="col-sm-2 col-form-label" for="basic-icon-default-fullname">Nama
-                                    Customer</label>
-                                <div class="col-sm-10">
-                                    <div class="input-group input-group-merge">
-                                        <span id="basic-icon-default-fullname2" class="input-group-text"><i
-                                                class="bx bx-user"></i></span>
-                                        <input type="text" class="form-control" id="basic-icon-default-fullname"
-                                            placeholder="Masukkan Nama..." aria-label="John Doe"
-                                            aria-describedby="basic-icon-default-fullname2" />
-                                    </div>
+    <div class="content-wrapper">
+        <!-- Content -->
+        <div class="container-xxl flex-grow-1 container-p-y">
+            <h4 class="fw-bold py-3 mb-4"><span class="text-muted fw-light">Profil /</span> Detail Profil</h4>
+
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="card border border-warning mb-4">
+                        <h5 class="card-header">Detail Profil</h5>
+                        <!-- Account -->
+                        <div class="card-body text-center">
+                            <div class="d-flex align-items-start align-items-sm-center">
+                                <div class="button-wrapper">
+                                    <h3 class="text-muted">Ari Gunawan Jatmiko</h3>
                                 </div>
                             </div>
-                            <div class="row mb-3">
-                                <label class="col-sm-2 col-form-label" for="basic-icon-default-company">Kode
-                                    Customer</label>
-                                <div class="col-sm-10">
-                                    <div class="input-group input-group-merge">
-                                        <span id="basic-icon-default-company2" class="input-group-text"><i
-                                                class="bx bx-buildings"></i></span>
-                                        <input type="text" id="basic-icon-default-company" class="form-control"
-                                            placeholder="Masukkan Kode Customer..." aria-label="ACME Inc."
-                                            aria-describedby="basic-icon-default-company2" />
-                                    </div>
+                            <div class="d-flex align-items-start align-items-sm-center gap-4">
+                                <img src="{{ asset('isi/assets/img/avatars/1.png') }}" alt="user-avatar"
+                                    class="d-block rounded" height="100" width="100" id="uploadedAvatar" />
+                                <div class="button-wrapper">
+                                    <p class="text-muted mb-5"><i class='bx bx-buildings'></i> Jl. Moch Juki</p>
+                                </div>
+                                <div class="button-wrapper">
+                                    <p class="text-muted mb-5"><i class='bx bx-envelope' ></i> arigunawanjatmiko@gmail.com</p>
+                                </div>
+                                <div class="button-wrapper">
+                                    <p class="text-muted mb-5"><i class='bx bx-phone' ></i> +62 857 </p>
                                 </div>
                             </div>
-                            <div class="row mb-3">
-                                <label class="col-sm-2 col-form-label" for="basic-icon-default-email">Alamat
-                                    Customer</label>
-                                <div class="col-sm-10">
-                                    <div class="input-group input-group-merge">
-                                        <span class="input-group-text"><i class="bx bx-envelope"></i></span>
-                                        <input type="text" id="basic-icon-default-email" class="form-control"
-                                            placeholder="Masukkan Alamat..." aria-label="john.doe"
-                                            aria-describedby="basic-icon-default-email2" />
-                                    </div>
+                            <div class="d-flex align-items-start align-items-sm-center gap-4">
+                                <div class="button-wrapper">
+                                    <a href="" class="btn btn-outline-primary mt-3"><i class='bx bxs-user-plus' ></i> Tambah Profil</a>
+                                </div>
+                                <div class="button-wrapper">
+                                    <a href="" class="btn btn-outline-danger mt-3"><i class='bx bxs-edit' ></i> Ganti Password</a>
                                 </div>
                             </div>
-                            <div class="row mb-3">
-                                <label class="col-sm-2 form-label" for="basic-icon-default-phone">Telepon
-                                    Customer</label>
-                                <div class="col-sm-10">
-                                    <div class="input-group input-group-merge">
-                                        <span id="basic-icon-default-phone2" class="input-group-text"><i
-                                                class="bx bx-phone"></i></span>
-                                        <input type="text" id="basic-icon-default-phone"
-                                            class="form-control phone-mask" placeholder="Masukkan Telepon..."
-                                            aria-label="658 799 8941" aria-describedby="basic-icon-default-phone2" />
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="row justify-content-end">
-                                <div class="col-sm-10">
-                                    <button type="submit" class="btn btn-primary">Send</button>
-                                </div>
-                            </div>
-                        </form>
+                        </div>
+                        <!-- /Account -->
                     </div>
-                </div>
-                <div class="modal-footer">
-                    <button type="button" class="btn btn-outline-secondary" data-bs-dismiss="modal">
-                        Close
-                    </button>
-                    <button type="button" class="btn btn-primary">Save changes</button>
+
                 </div>
             </div>
         </div>
+
+        {{-- ISI MODAL DISINI --}}
     </div>
-    {{-- MODAL CLOSE --}}
-</div>
 @endsection
